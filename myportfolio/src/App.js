2ad373e9c4b77b5e103eef2c8d5fb80db84f3f4d
Diane Candler <dianedcandler@gmail.com>
2020-07-24 16:16:56 -0500

    updating files with set-up content and CSS

---
+++ myportfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import NavIndication from './components/Nav'
+
+function App() {
+  return (
+    <div>
+      <NavIndication name="Diane Candler"/>
+      <h2>My Portfolio</h2>
+    </div>
+  );
+}
+
+export default App;
