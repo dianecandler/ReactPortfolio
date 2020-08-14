bd28da6cf328c347e36c3a543f7d798224ff5f96
Diane Candler <dianedcandler@gmail.com>
2020-08-14 10:41:28 -0500

    added build

---
+++ build/precache-manifest.8026804530ef9ba466df90ccd219cf86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7996ca0684c9483fa5265698023b8bb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b35063bc632fb0173b06",
+    "url": "/static/css/2.c3594674.chunk.css"
+  },
+  {
+    "revision": "b35063bc632fb0173b06",
+    "url": "/static/js/2.2ca3ac51.chunk.js"
+  },
+  {
+    "revision": "bac08d190ad2c3782685257760a40a06",
+    "url": "/static/js/2.2ca3ac51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e50be80aedf6c2eb3c4",
+    "url": "/static/js/main.d49e7b15.chunk.js"
+  },
+  {
+    "revision": "dd0ad302ca727c93b192",
+    "url": "/static/js/runtime-main.4e7c19c6.js"
+  },
+  {
+    "revision": "893bb1afa6914240aa42d7877a7190da",
+    "url": "/static/media/Resume.893bb1af.pdf"
+  }
+]);
